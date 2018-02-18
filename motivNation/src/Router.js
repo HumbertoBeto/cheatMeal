ce61d18bc0340d5e972f6e3bd3c28fd591a041f0
HumbertoBeto <hhernandez16@mail.csuchico.edu>
2018-02-18 15:02:06 -0800

    Added Register view and home screen transitions

---
+++ motivNation/src/Router.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Home from './components/Home';
+
+const RouterComponent = () => {
+  return (
+    // login scene
+    <Router>
+      <Scene key="root" hideNavBar>
+      <Scene key="auth">
+        <Scene
+        rightTitle="Register?"
+        onRight={() => Actions.register()}
+        key="login"
+        component={LoginForm}
+        title=" Welcome to MotivNation!"
+        initial
+        />
+        <Scene key="register" component={RegisterForm} title="Register Here" />
+      </Scene>
+      <Scene key="main">
+        <Scene key="home" component={Home} title="Home" />
+      </Scene>
+      </Scene>
+    </Router>
+  );
+}
+
+export default RouterComponent;
---
+++ motivNation/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text> My Goals </Text>
+        <Text> My Tools </Text>
+        <Text> My Nation </Text>
+      </View>
+    );
+  }
+}
+
+export default Home;
